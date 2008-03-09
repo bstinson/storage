82071852309fe40ae21ef8f936b39cb6d1f576ee
brad <brad>
2008-03-09 20:13:30 +0000

    Added contact info for company

--- db/migrate/002_create_companies.rb
+++ db/migrate/002_create_companies.rb
@@ -2,6 +2,11 @@ class CreateCompanies < ActiveRecord::Migration
   def self.up
     create_table :companies do |t|
       t.string :name
+      t.string :address
+      t.string :city
+      t.string :state
+      t.string :zip
+      t.string :phone
       t.timestamps
     end
   end
