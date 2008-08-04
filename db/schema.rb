7d0ffc4deb7f93e440a1b7e5e0a751094e1c96b5
brad <brad>
2008-08-04 18:25:57 +0000

    Added total to bottom of customer_report.

--- db/schema.rb
+++ db/schema.rb
@@ -1,5 +1,5 @@
 # This file is auto-generated from the current state of the database. Instead of editing this file, 
-# please use the migrations feature of ActiveRecord to incrementally modify your database, and
+# please use the migrations feature of Active Record to incrementally modify your database, and
 # then regenerate this schema definition.
 #
 # Note that this schema.rb definition is the authoritative source for your database schema. If you need
@@ -13,7 +13,7 @@ ActiveRecord::Schema.define(:version => 11) do
 
   create_table "buildings", :force => true do |t|
     t.string   "name"
-    t.integer  "company_id"
+    t.integer  "company_id", :limit => 11
     t.datetime "created_at"
     t.datetime "updated_at"
   end
@@ -31,11 +31,11 @@ ActiveRecord::Schema.define(:version => 11) do
 
   create_table "notes", :force => true do |t|
     t.string   "note"
-    t.integer  "unit_id"
-    t.integer  "user_id"
+    t.integer  "unit_id",         :limit => 11
+    t.integer  "user_id",         :limit => 11
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "prevcustomer_id"
+    t.integer  "prevcustomer_id", :limit => 11
   end
 
   create_table "prevcustomers", :force => true do |t|
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(:version => 11) do
   end
 
   create_table "sessions", :force => true do |t|
-    t.string   "session_id", :default => "", :null => false
+    t.string   "session_id", :null => false
     t.text     "data"
     t.datetime "created_at"
     t.datetime "updated_at"
@@ -68,10 +68,10 @@ ActiveRecord::Schema.define(:version => 11) do
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
 
   create_table "units", :force => true do |t|
-    t.integer  "unit_num"
-    t.integer  "width"
-    t.integer  "height"
-    t.decimal  "monthly_price", :precision => 8, :scale => 2
+    t.integer  "unit_num",      :limit => 11
+    t.integer  "width",         :limit => 11
+    t.integer  "height",        :limit => 11
+    t.decimal  "monthly_price",               :precision => 8, :scale => 2
     t.string   "name"
     t.string   "ssn"
     t.string   "dl"
@@ -87,18 +87,18 @@ ActiveRecord::Schema.define(:version => 11) do
     t.string   "auth_users"
     t.string   "code"
     t.string   "status"
-    t.integer  "building_id"
-    t.integer  "company_id"
+    t.integer  "building_id",   :limit => 11
+    t.integer  "company_id",    :limit => 11
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.decimal  "deposit",       :precision => 8, :scale => 2, :default => 0.0
+    t.decimal  "deposit",                     :precision => 8, :scale => 2, :default => 0.0
   end
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.string   "password"
-    t.integer  "company_id", :null => false
+    t.integer  "company_id", :limit => 11, :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
